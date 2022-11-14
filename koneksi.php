85585b343082015179c41e773974f408c01f0096
lilbroccy <hendykuyy96@gmail.com>
2022-11-14 12:22:15 +0700

    Update

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $server 	= "localhost";
 $username 	= "root";
 $password 	= "";
-$db 		= "yooksbakery";
+$db 		= "yooks_bakery";
 $koneksi = mysqli_connect($server, $username, $password, $db);
 
 if (mysqli_connect_errno()) {
