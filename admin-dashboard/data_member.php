537c0cfbd3fcb64e39db4376a3a1eac6679af13a
lilbroccy <hendykuyy96@gmail.com>
2022-11-30 22:15:55 +0700

    update

--- admin-dashboard/data_member.php
+++ admin-dashboard/data_member.php
@@ -156,18 +156,25 @@ $sesLvl = $_SESSION['level'];
                         <li class="sidebar-item">
                             <a class="sidebar-link waves-effect waves-dark sidebar-link" href="data_member.php"
                                 aria-expanded="false">
-                                <i class="fa fa-table" aria-hidden="true"></i>
+                                <i class="fa fa-address-card" aria-hidden="true"></i>
                                 <span class="hide-menu">Data Member</span>
                             </a>
                         </li>
                         <li class="sidebar-item">
-                            <a class="sidebar-link waves-effect waves-dark sidebar-link" href="fontawesome.html"
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link" href="data_produk.php"
                                 aria-expanded="false">
-                                <i class="fa fa-font" aria-hidden="true"></i>
-                                <span class="hide-menu">Icon</span>
+                                <i class="fa fa-table" aria-hidden="true"></i>
+                                <span class="hide-menu">Data Produk</span>
                             </a>
                         </li>
                         <li class="sidebar-item">
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link" href="orderan.php"
+                                aria-expanded="false">
+                                <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                                <span class="hide-menu">Orderan</span>
+                            </a>
+                        </li>
+                        <!--<li class="sidebar-item">
                             <a class="sidebar-link waves-effect waves-dark sidebar-link" href="map-google.html"
                                 aria-expanded="false">
                                 <i class="fa fa-globe" aria-hidden="true"></i>
@@ -187,6 +194,11 @@ $sesLvl = $_SESSION['level'];
                                 <i class="fa fa-info-circle" aria-hidden="true"></i>
                                 <span class="hide-menu">Error 404</span>
                             </a>
+                        </li>-->
+                        <li class="text-center p-20 upgrade-btn">
+                            <a href="/YooksBakery/user-dashboard/login.php"
+                                class="btn d-grid btn-danger text-white" target="_blank">
+                                Logout</a>
                         </li>
                     </ul>
 
@@ -217,7 +229,7 @@ $sesLvl = $_SESSION['level'];
                             </ol>
                             <a href="https://www.wrappixel.com/templates/ampleadmin/" target="_blank"
                                 class="btn btn-danger  d-none d-md-block pull-right ms-3 hidden-xs hidden-sm waves-effect waves-light text-white">Upgrade
-                                to Pro</a>
+                                to</a>
                         </div>
                     </div> -->
                 </div>
