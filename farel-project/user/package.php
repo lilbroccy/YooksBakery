6b012fc9f8b319f31f8304b23a3b72bba8291f90
andrianaputridevi <117130049+andrianaputridevi@users.noreply.github.com>
2022-12-15 18:56:52 +0700

    Update package.php

--- farel-project/user/package.php
+++ farel-project/user/package.php
@@ -49,63 +49,119 @@
     <div class="container-fluid py-5">
         <div class="container pt-5 pb-3">
             <div class="text-center mb-3 pb-3">
-                <h6 class="text-primary text-uppercase" style="letter-spacing: 5px;">Destination</h6>
-                <h1>Explore Top Destination</h1>
+                <h6 class="text-primary text-uppercase" style="letter-spacing: 5px;">Jenis Produk Kami</h6>
+                <h1>Cake</h1>
+                
             </div>
             <div class="row">
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="destination-item position-relative overflow-hidden mb-2">
-                        <img class="img-fluid" src="img/destination-1.jpg" alt="">
-                        <a class="destination-overlay text-white text-decoration-none" href="">
-                            <h5 class="text-white">United States</h5>
-                            <span>100 Cities</span>
-                        </a>
+                        <img src="img/cake12.jpg" alt="" width="250px">
                     </div>
+                    <div class="col-lg-4">
+            <div class="portfolio-info">
+              <h3>Dark Chocolate</h3>
+              <ul>
+                <li><strong>Harga </strong><br>Rp90.000</li>
+                <br>
+                <li><strong>Deskripsi </strong><br>
+                  <p>cape
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+                </div>
+                
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="destination-item position-relative overflow-hidden mb-2">
+                        <img src="img/cake1.jpg" alt=""  width="250px">
+                    </div>
+                    <div class="col-lg-4">
+            <div class="portfolio-info">
+              <h3>Dark Chocolate</h3>
+              <ul>
+                <li><strong>Harga </strong><br>Rp90.000</li>
+                <br>
+                <li><strong>Deskripsi </strong><br>
+                  <p>cape
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="destination-item position-relative overflow-hidden mb-2">
-                        <img class="img-fluid" src="img/destination-2.jpg" alt="">
-                        <a class="destination-overlay text-white text-decoration-none" href="">
-                            <h5 class="text-white">United Kingdom</h5>
-                            <span>100 Cities</span>
-                        </a>
+                        <img src="img/cake3.jpg" alt="" width="250">
                     </div>
+                    <div class="col-lg-4">
+            <div class="portfolio-info">
+              <h3>Dark Chocolate</h3>
+              <ul>
+                <li><strong>Harga </strong><br>Rp90.000</li>
+                <br>
+                <li><strong>Deskripsi </strong><br>
+                  <p>cape
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="destination-item position-relative overflow-hidden mb-2">
-                        <img class="img-fluid" src="img/destination-3.jpg" alt="">
-                        <a class="destination-overlay text-white text-decoration-none" href="">
-                            <h5 class="text-white">Australia</h5>
-                            <span>100 Cities</span>
-                        </a>
+                        <img src="img/cake4.jpg" alt="" width="250px">
                     </div>
+                    <div class="col-lg-4">
+            <div class="portfolio-info">
+              <h3>Dark Chocolate</h3>
+              <ul>
+                <li><strong>Harga </strong><br>Rp90.000</li>
+                <br>
+                <li><strong>Deskripsi </strong><br>
+                  <p>cape
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="destination-item position-relative overflow-hidden mb-2">
-                        <img class="img-fluid" src="img/destination-4.jpg" alt="">
-                        <a class="destination-overlay text-white text-decoration-none" href="">
-                            <h5 class="text-white">India</h5>
-                            <span>100 Cities</span>
-                        </a>
+                        <img src="img/cake5.jpg" alt="" width="250px">
                     </div>
+                    <div class="col-lg-4">
+            <div class="portfolio-info">
+              <h3>Dark Chocolate</h3>
+              <ul>
+                <li><strong>Harga </strong><br>Rp90.000</li>
+                <br>
+                <li><strong>Deskripsi </strong><br>
+                  <p>cape
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="destination-item position-relative overflow-hidden mb-2">
-                        <img class="img-fluid" src="img/destination-5.jpg" alt="">
-                        <a class="destination-overlay text-white text-decoration-none" href="">
-                            <h5 class="text-white">South Africa</h5>
-                            <span>100 Cities</span>
-                        </a>
+                        <img src="img/cake6.jpg" alt="" width="250x">
                     </div>
-                </div>
-                <div class="col-lg-4 col-md-6 mb-4">
-                    <div class="destination-item position-relative overflow-hidden mb-2">
-                        <img class="img-fluid" src="img/destination-6.jpg" alt="">
-                        <a class="destination-overlay text-white text-decoration-none" href="">
-                            <h5 class="text-white">Indonesia</h5>
-                            <span>100 Cities</span>
-                        </a>
-                    </div>
+                    <div class="col-lg-4">
+            <div class="portfolio-info">
+              <h3>Dark Chocolate</h3>
+              <ul>
+                <li><strong>Harga </strong><br>Rp90.000</li>
+                <br>
+                <li><strong>Deskripsi </strong><br>
+                  <p>cape
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
                 </div>
             </div>
         </div>
