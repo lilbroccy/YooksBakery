855ffdab2be524e6bcf82f404af7e55a665c801a
lilbroccy <hendykuyy96@gmail.com>
2023-01-02 15:35:24 +0700

    hapus yang tidak penting

--- admin-dashboard/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-require('koneksi.php');
-$id = $_GET['id_user'];
-mysqli_query($koneksi, "DELETE FROM user_detail WHERE id_user='$id'") or die(mysql_error());
-header("location: /YooksBakery/admin-dashboard/data_member.php")
-?>
